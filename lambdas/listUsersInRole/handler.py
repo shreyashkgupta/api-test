71bbc6ebb26bc066f1a0e2fbc9c8fc69e0892858
Shreyash Gupta <48386323+shreyashkgupta@users.noreply.github.com>
2023-12-29 00:02:57 +0530

    Delete lambdas directory

--- lambdas/listUsersInRole/handler.py
+++
@@ -1,9 +0,0 @@
-import boto3
-
-def lambda_handler(event, context):
-    iam = boto3.client('iam')
-    role_name = "YOUR_ROLE_NAME"
-    response = iam.list_users_for_iam_role(RoleName=role_name)
-    users = response['Users']
-    for user in users:
-        print(user['UserName'])
