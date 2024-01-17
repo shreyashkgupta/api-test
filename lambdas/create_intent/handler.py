252d7e331069c2fbc0183a7106d7347f035909d3
Shreyash Gupta <48386323+shreyashkgupta@users.noreply.github.com>
2024-01-18 03:27:33 +0530

    Delete lambdas directory

--- lambdas/create_intent/handler.py
+++
@@ -1,9 +0,0 @@
-import boto3
-
-def create_intent(intent_name, slots):
-    client = boto3.client('lex-models')
-    response = client.put_intent(
-        name=intent_name,
-        slots=slots
-    )
-    return response
