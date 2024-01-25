e14f2b19e9d47e02cddd6c514c39a98f5a0ac92c
Shreyash Gupta <48386323+shreyashkgupta@users.noreply.github.com>
2024-01-25 14:06:47 +0530

    Delete lambdas directory

--- lambdas/get_message/handler.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def lambda_handler(event, context):
-    message = "Hello world!"
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(message)
-    }
-    return response
