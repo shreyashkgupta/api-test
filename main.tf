dceb45295a8292b27772a4a0d18121633bcafc55
shreyashkgupta <autobot@cloudcode.ai>
2024-01-26 15:01:22 +0000

    basic template

--- main.tf
+++ main.tf
@@ -8,9 +8,15 @@ terraform {
   }
 }
 
+variable "project_id" {
+    type = string
+    description = "Project ID"
+}
+
 provider "google" {
   region = "us-west2"
   zone = "us-west2a"
+  provider = var.project_id
 }
 
 
