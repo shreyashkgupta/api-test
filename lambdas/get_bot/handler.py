37909a9689b770ee451b60ce45fc3b26567e7796
Shreyash Gupta <48386323+shreyashkgupta@users.noreply.github.com>
2024-01-29 22:58:22 +0530

    Delete lambdas directory

--- lambdas/get_bot/handler.py
+++
@@ -1,9 +0,0 @@
-import boto3
-
-def lambda_handler(event, context):
-    client = boto3.client('lex-models')
-    response = client.get_bot(
-        name='chatbot-name',
-        versionOrAlias='$LATEST'
-    )
-    return response
