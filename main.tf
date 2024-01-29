87fb3e682fba3d34fccdfba8eb295e71e6a726a4
shreyashkgupta <autobot@cloudcode.ai>
2024-01-29 16:17:30 +0000

    basic template

--- main.tf
+++ main.tf
@@ -27,12 +27,12 @@ resource "google_firestore_database" "work-dev-user-data" {
   type                              = "FIRESTORE_NATIVE"
 }
 
-resource "random_id" "work-Dev-create_user-randomID" {
+resource "random_id" "Dev-create_user-randomID" {
   byte_length = 8
 }
 
-resource "google_storage_bucket" "work-Dev-create_user-bucket" {
-  name                        = "${random_id.work-Dev-create_user-randomID.hex}-gcf-source"
+resource "google_storage_bucket" "Dev-create_user-bucket" {
+  name                        = "${random_id.Dev-create_user-randomID.hex}-gcf-source"
   location                    = "US"
   uniform_bucket_level_access = true
 }
@@ -43,7 +43,7 @@ source_dir  = "cloudfunctions/create_user"
 output_path = "cloudfunctions/create_user.zip"
 }
 
-resource "google_storage_bucket_object" "work-Dev-create_user-object" {
+resource "google_storage_bucket_object" "Dev-create_user-object" {
   name   = "Dev-create_user-source.zip"
   bucket = google_storage_bucket.work-Dev-create_user-bucket.name
   source = data.archive_file.zip_the_python_code_Dev_create_user.output_path # Add path to the zipped function source code
@@ -71,29 +71,29 @@ resource "google_storage_bucket_object" "work-Dev-create_user-object" {
 # }
 
 resource "google_cloudfunctions_function" "cf_Dev_create_user" {
-  name        = "work-Dev-create_user"
+  name        = "cf_Dev-create_user"
   runtime     = "python39"
 
-  source_archive_bucket = google_storage_bucket.work-Dev-create_user-bucket.name
-  source_archive_object = google_storage_bucket_object.work-Dev-create_user-object.name
+  source_archive_bucket = google_storage_bucket.Dev-create_user-bucket.name
+  source_archive_object = google_storage_bucket_object.Dev-create_user-object.name
   available_memory_mb = "256"
   timeout = 60
   max_instances = 1
 }
 
 resource "google_cloud_run_service_iam_member" "Dev_create_user_member" {
-  location = google_cloudfunctions2_function.cf_Dev_create_user.location
-  service  = google_cloudfunctions2_function.cf_Dev_create_user.name
+  location = google_cloudfunctions_function.cf_Dev_create_user.location
+  service  = google_cloudfunctions_function.cf_Dev_create_user.name
   role     = "roles/run.invoker"
   member   = "allUsers"
 }
 
-resource "random_id" "work-Dev-get_user-randomID" {
+resource "random_id" "Dev-get_user-randomID" {
   byte_length = 8
 }
 
-resource "google_storage_bucket" "work-Dev-get_user-bucket" {
-  name                        = "${random_id.work-Dev-get_user-randomID.hex}-gcf-source"
+resource "google_storage_bucket" "Dev-get_user-bucket" {
+  name                        = "${random_id.Dev-get_user-randomID.hex}-gcf-source"
   location                    = "US"
   uniform_bucket_level_access = true
 }
@@ -104,7 +104,7 @@ source_dir  = "cloudfunctions/get_user"
 output_path = "cloudfunctions/get_user.zip"
 }
 
-resource "google_storage_bucket_object" "work-Dev-get_user-object" {
+resource "google_storage_bucket_object" "Dev-get_user-object" {
   name   = "Dev-get_user-source.zip"
   bucket = google_storage_bucket.work-Dev-get_user-bucket.name
   source = data.archive_file.zip_the_python_code_Dev_get_user.output_path # Add path to the zipped function source code
@@ -132,29 +132,29 @@ resource "google_storage_bucket_object" "work-Dev-get_user-object" {
 # }
 
 resource "google_cloudfunctions_function" "cf_Dev_get_user" {
-  name        = "work-Dev-get_user"
+  name        = "cf_Dev-get_user"
   runtime     = "python39"
 
-  source_archive_bucket = google_storage_bucket.work-Dev-get_user-bucket.name
-  source_archive_object = google_storage_bucket_object.work-Dev-get_user-object.name
+  source_archive_bucket = google_storage_bucket.Dev-get_user-bucket.name
+  source_archive_object = google_storage_bucket_object.Dev-get_user-object.name
   available_memory_mb = "256"
   timeout = 60
   max_instances = 1
 }
 
 resource "google_cloud_run_service_iam_member" "Dev_get_user_member" {
-  location = google_cloudfunctions2_function.cf_Dev_get_user.location
-  service  = google_cloudfunctions2_function.cf_Dev_get_user.name
+  location = google_cloudfunctions_function.cf_Dev_get_user.location
+  service  = google_cloudfunctions_function.cf_Dev_get_user.name
   role     = "roles/run.invoker"
   member   = "allUsers"
 }
 
-resource "random_id" "work-Dev-update_user-randomID" {
+resource "random_id" "Dev-update_user-randomID" {
   byte_length = 8
 }
 
-resource "google_storage_bucket" "work-Dev-update_user-bucket" {
-  name                        = "${random_id.work-Dev-update_user-randomID.hex}-gcf-source"
+resource "google_storage_bucket" "Dev-update_user-bucket" {
+  name                        = "${random_id.Dev-update_user-randomID.hex}-gcf-source"
   location                    = "US"
   uniform_bucket_level_access = true
 }
@@ -165,7 +165,7 @@ source_dir  = "cloudfunctions/update_user"
 output_path = "cloudfunctions/update_user.zip"
 }
 
-resource "google_storage_bucket_object" "work-Dev-update_user-object" {
+resource "google_storage_bucket_object" "Dev-update_user-object" {
   name   = "Dev-update_user-source.zip"
   bucket = google_storage_bucket.work-Dev-update_user-bucket.name
   source = data.archive_file.zip_the_python_code_Dev_update_user.output_path # Add path to the zipped function source code
@@ -193,29 +193,29 @@ resource "google_storage_bucket_object" "work-Dev-update_user-object" {
 # }
 
 resource "google_cloudfunctions_function" "cf_Dev_update_user" {
-  name        = "work-Dev-update_user"
+  name        = "cf_Dev-update_user"
   runtime     = "python39"
 
-  source_archive_bucket = google_storage_bucket.work-Dev-update_user-bucket.name
-  source_archive_object = google_storage_bucket_object.work-Dev-update_user-object.name
+  source_archive_bucket = google_storage_bucket.Dev-update_user-bucket.name
+  source_archive_object = google_storage_bucket_object.Dev-update_user-object.name
   available_memory_mb = "256"
   timeout = 60
   max_instances = 1
 }
 
 resource "google_cloud_run_service_iam_member" "Dev_update_user_member" {
-  location = google_cloudfunctions2_function.cf_Dev_update_user.location
-  service  = google_cloudfunctions2_function.cf_Dev_update_user.name
+  location = google_cloudfunctions_function.cf_Dev_update_user.location
+  service  = google_cloudfunctions_function.cf_Dev_update_user.name
   role     = "roles/run.invoker"
   member   = "allUsers"
 }
 
-resource "random_id" "work-Dev-delete_user-randomID" {
+resource "random_id" "Dev-delete_user-randomID" {
   byte_length = 8
 }
 
-resource "google_storage_bucket" "work-Dev-delete_user-bucket" {
-  name                        = "${random_id.work-Dev-delete_user-randomID.hex}-gcf-source"
+resource "google_storage_bucket" "Dev-delete_user-bucket" {
+  name                        = "${random_id.Dev-delete_user-randomID.hex}-gcf-source"
   location                    = "US"
   uniform_bucket_level_access = true
 }
@@ -226,7 +226,7 @@ source_dir  = "cloudfunctions/delete_user"
 output_path = "cloudfunctions/delete_user.zip"
 }
 
-resource "google_storage_bucket_object" "work-Dev-delete_user-object" {
+resource "google_storage_bucket_object" "Dev-delete_user-object" {
   name   = "Dev-delete_user-source.zip"
   bucket = google_storage_bucket.work-Dev-delete_user-bucket.name
   source = data.archive_file.zip_the_python_code_Dev_delete_user.output_path # Add path to the zipped function source code
@@ -254,19 +254,19 @@ resource "google_storage_bucket_object" "work-Dev-delete_user-object" {
 # }
 
 resource "google_cloudfunctions_function" "cf_Dev_delete_user" {
-  name        = "work-Dev-delete_user"
+  name        = "cf_Dev-delete_user"
   runtime     = "python39"
 
-  source_archive_bucket = google_storage_bucket.work-Dev-delete_user-bucket.name
-  source_archive_object = google_storage_bucket_object.work-Dev-delete_user-object.name
+  source_archive_bucket = google_storage_bucket.Dev-delete_user-bucket.name
+  source_archive_object = google_storage_bucket_object.Dev-delete_user-object.name
   available_memory_mb = "256"
   timeout = 60
   max_instances = 1
 }
 
 resource "google_cloud_run_service_iam_member" "Dev_delete_user_member" {
-  location = google_cloudfunctions2_function.cf_Dev_delete_user.location
-  service  = google_cloudfunctions2_function.cf_Dev_delete_user.name
+  location = google_cloudfunctions_function.cf_Dev_delete_user.location
+  service  = google_cloudfunctions_function.cf_Dev_delete_user.name
   role     = "roles/run.invoker"
   member   = "allUsers"
 }
