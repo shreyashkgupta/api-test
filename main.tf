3eb1dea93f2892c8337867ddb714b2295c330968
shreyashkgupta <autobot@cloudcode.ai>
2024-01-29 13:47:13 +0000

    basic template

--- main.tf
+++ main.tf
@@ -26,3 +26,38 @@ resource "google_firestore_database" "gcp-new-test-dev-users" {
   location_id                       = "nam5"
   type                              = "FIRESTORE_NATIVE"
 }
+
+resource "google_cloud_run_service_iam_member" "member" {
+  location = google_cloudfunctions2_function.cf_Dev_Create User.location
+  service  = google_cloudfunctions2_function.cf_Dev_Create User.name
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
+
+resource "google_cloud_run_service_iam_member" "member" {
+  location = google_cloudfunctions2_function.cf_Dev_Get User.location
+  service  = google_cloudfunctions2_function.cf_Dev_Get User.name
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
+
+resource "google_cloud_run_service_iam_member" "member" {
+  location = google_cloudfunctions2_function.cf_Dev_Update User.location
+  service  = google_cloudfunctions2_function.cf_Dev_Update User.name
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
+
+resource "google_cloud_run_service_iam_member" "member" {
+  location = google_cloudfunctions2_function.cf_Dev_Delete User.location
+  service  = google_cloudfunctions2_function.cf_Dev_Delete User.name
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
+
+resource "google_cloud_run_service_iam_member" "member" {
+  location = google_cloudfunctions2_function.cf_Dev_List Users.location
+  service  = google_cloudfunctions2_function.cf_Dev_List Users.name
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
