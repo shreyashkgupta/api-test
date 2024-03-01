441ba70ff9679ac9d83ccad66d4feade6f363779
Shreyash Gupta <48386323+shreyashkgupta@users.noreply.github.com>
2024-03-02 02:10:26 +0530

    Delete lambdas directory

--- lambdas/list_users/handler.py
+++
@@ -1,7 +0,0 @@
-import boto3
-
-def lambda_handler(event, context):
-    client = boto3.client('iam')
-    response = client.list_users()
-    for user in response['Users']:
-        print(user['UserName'])
