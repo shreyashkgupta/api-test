2d0196ac29c0be007edfe115d6204c3615b27eb9
Shreyash Gupta <48386323+shreyashkgupta@users.noreply.github.com>
2024-03-03 17:49:16 +0530

    Delete cloudfunctions directory

--- cloudfunctions/get-user/main.py
+++
@@ -1,4 +0,0 @@
-def main(request):
-    user_id = request.args.get('user_id')
-    # code to retrieve user from the system
-    return f"User with id {user_id} retrieved successfully"
--- cloudfunctions/get-users/main.py
+++
@@ -1,3 +0,0 @@
-def main(request):
-    users = ["user1", "user2", "user3"] # replace this with your code to get all users
-    return {'users': users}
