9631b7479ccc7029f4da9ca189c2e5e8abdafb55
Shreyash Gupta <48386323+shreyashkgupta@users.noreply.github.com>
2024-03-03 14:24:19 +0530

    Delete cloudfunctions directory

--- cloudfunctions/create-user/main.py
+++
@@ -1,8 +0,0 @@
-def main(request):
-    if request.method == 'POST':
-        # Get user data from request body
-        user_data = request.get_json()
-        # TODO: Add code to create new user with user_data
-        return 'User created successfully!'
-    else:
-        return 'Error: Invalid request method. Only POST requests are allowed.'
