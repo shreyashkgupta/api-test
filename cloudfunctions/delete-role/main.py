5b787d8928c1f6b7ebd1eac954dee4b2852625a7
Shreyash Gupta <48386323+shreyashkgupta@users.noreply.github.com>
2024-03-03 20:39:48 +0530

    Delete cloudfunctions directory

--- cloudfunctions/delete-role/main.py
+++
@@ -1,9 +0,0 @@
-def main(request):
-    from google.cloud import datastore
-
-    client = datastore.Client()
-
-    role_key = client.key('Role', request.json['id'])
-    client.delete(role_key)
-
-    return f"Role {request.json['id']} deleted successfully."
