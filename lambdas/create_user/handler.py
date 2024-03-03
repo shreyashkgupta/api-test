9436702b4c3a27bc686d6df40d99d775b40b34a8
Shreyash Gupta <48386323+shreyashkgupta@users.noreply.github.com>
2024-03-03 21:01:28 +0530

    Delete lambdas directory

--- lambdas/create_user/handler.py
+++
@@ -1,6 +0,0 @@
-import boto3
-
-def create_user(username, password):
-    iam = boto3.client('iam')
-    iam.create_user(UserName=username)
-    iam.create_login_profile(UserName=username, Password=password)
