28495170c2d2bf633da6f4acaca35e8b9403d280
Shreyash Gupta <48386323+shreyashkgupta@users.noreply.github.com>
2024-03-26 01:42:43 +0530

    Delete lambdas directory

--- lambdas/delete_chatbot/handler.py
+++
@@ -1,5 +0,0 @@
-import boto3
-
-def delete_chatbot(chatbot_name):
-    client = boto3.client('lex-models')
-    client.delete_bot(name=chatbot_name)
--- lambdas/list_chatbots/handler.py
+++
@@ -1,8 +0,0 @@
-import boto3
-
-def lambda_handler(event, context):
-    client = boto3.client('lex-models')
-    response = client.get_bots()
-    bots = response['bots']
-    bot_names = [bot['name'] for bot in bots]
-    return bot_names
